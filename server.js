d20564098606747fa744dd60c8855ea4efbb6457
lydiahu8 <lydiahu8@gmail.com>
2019-03-08 20:31:36 -0800

    Add reviews service to proxy server

--- server.js
+++ server.js
@@ -5,6 +5,7 @@ const path = require('path');
 const favicon = require('serve-favicon')
 
 const app = express();
+const port = 8080;
 
 app.use(cors());
 
@@ -14,12 +15,12 @@ app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')))
 
 app.use('/files', express.static(`${__dirname}/public`));
 
-app.get('/products/:id', (req, res) => {
+app.get('/:id', (req, res) => {
   res.sendFile(`${__dirname}/public/index.html`);
 });
 
 app.get('*', (req, res) => {
-  res.redirect('/products/1');
+  res.redirect('/1');
 })
 
-app.listen(80);
+app.listen(port, () => console.log(`Now listening on port ${port}!`));
