17085037a71a2dbdfaceb82f58d1ab3f6ee2e1a0
lydiahu8 <lydiahu8@gmail.com>
2019-03-12 12:02:56 -0700

    Update EC2 links for related and products microservices

--- server.js
+++ server.js
@@ -21,7 +21,7 @@ app.use('/api/reviews', proxy({
   target: 'http://ec2-18-209-30-138.compute-1.amazonaws.com'
 }));
 // app.use('/api/related', proxy({
-//   target: 'http://ec2-54-219-186-15.us-west-1.compute.amazonaws.com/9287120'
+//   target: 'http://ec2-54-219-186-15.us-west-1.compute.amazonaws.com'
 // }));
 // app.use('/api/products', proxy({
 //   target: 'http://ec2-3-90-25-170.compute-1.amazonaws.com'
