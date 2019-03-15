db9fc171825297362834ad6da144ca7e94501bc8
lydiahu8 <lydiahu8@gmail.com>
2019-03-15 14:39:52 -0700

    Update all microservices to load balancer ec2

--- server.js
+++ server.js
@@ -32,10 +32,10 @@ app.use('/api/related', proxy({
   target: 'http://ec2-54-153-105-133.us-west-1.compute.amazonaws.com'
 }));
 app.use('/api/products', proxy({
-  target: 'http://ec2-54-198-94-221.compute-1.amazonaws.com'
+  target: 'http://ec2-3-94-89-165.compute-1.amazonaws.com'
 }));
 app.use('/api', proxy({
-  target: 'http://ec2-54-175-87-50.compute-1.amazonaws.com'
+  target: 'http://ec2-54-211-88-14.compute-1.amazonaws.com'
 }));
 
 app.get('/:id', (req, res) => {
